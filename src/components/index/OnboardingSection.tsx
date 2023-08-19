0afe01a6000b2e7809dfbefa02a705b5ce134cbf
abundiko <abundiko111@gmail.com>
2023-08-20 00:00:38 +0100

    started working on OnboardingSection and added a push script

--- src/components/index/OnboardingSection.tsx
+++ src/components/index/OnboardingSection.tsx
@@ -13,15 +13,16 @@ const OnboardingSection = () => {
           Creating
         </p>
         <div className="w-full ">
-          <div className="flex items-center">
-            <div className="w-16 flex-shrink-0 flex justify-end items-center h-full">
-              <div className="w-12 rounded-full aspect-square shadow app-shadows app-borders border flex justify-center items-center">
+          <div className="flex h-fit items-center relative">
+            <div className="w-12 flex-shrink-0 flex-grow flex relative items-center h-full">
+              <div className="w-[2px] h-full absolute left-1/2 -translate-x-1/2 app-theme-opposite app-bg-opacity" />
+              <div className="w-12 text-lg font-bold rounded-full relative app-theme aspect-square shadow app-shadows app-borders border flex justify-center items-center">
                 1
               </div>
             </div>
-            <div className="w-full flex-shrink flex items-center">
-              <div className="w-20 h-[2px] app-theme-opposite" />
-              <div className="app-theme-opposite p-3 rounded-md">
+            <div className="w-full flex-shrink flex items-center py-3">
+              <div className="w-20 h-[2px] app-theme-opposite app-bg-opacity" />
+              <div className="app-theme-opposite p-3 rounded-md app-bg-opacity">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
                 beatae a ex maxime labore fugiat, veniam minus voluptas corrupti
                 esse.
