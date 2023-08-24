25f01b3c9e71d8ae48096355cf0e7a0830b440f1
abundiko <abundiko111@gmail.com>
2023-08-24 05:08:06 +0100

    fixed getUser bug

--- src/server/auth/getUser.ts
+++ src/server/auth/getUser.ts
@@ -6,33 +6,31 @@ import { connectDB } from "../mongoose/init";
 import UserModel, { UserDocument } from "../mongoose/schemas/userSchema";
 
 export default async function getUser(
-  _id: string|null = null
+  _id: string | null = null
 ): Promise<UserDocument | string> {
   await connectDB();
-  if(!_id){
-     const cookiesStore = cookies();
-  const token = cookiesStore.get("_uid");
-  const secret = process.env.JWT_SECRET || "";
-  if (!token) return "here 1";
-  jwt.verify(token.value, secret, async (error, decoded) => {
-    if (error) {
+  if (!_id) {
+    const token = cookies().get("_uid");
+    const secret = process.env.JWT_SECRET || "";
+    if (!token) {
+      return "here 1";
+    }
+    try {
+      const decoded = jwt.verify(token.value, secret) as any;
+      const uid = decoded?._uid;
+      const userData: UserDocument | null = await UserModel.findById(uid);
+      console.log(userData);
+      if (userData) {
+        return userData;
+      } else {
+        return "here 3";
+      }
+    } catch (error) {
       console.error("Failed to verify the token:", error);
       return "here 2";
-    } else {
-      const uid = (decoded as any)?._uid;
-      const userData:UserDocument|null = await UserModel.findById(uid);
-      console.log(userData);
-
-      if (userData){
-        return userData;
-      } else
-      return "here 3";
     }
-
-  });
-  return "here 3.5"
-  }else{
-  const userDoc: UserDocument | null = await UserModel.findById(_id);
-  return userDoc != null ? userDoc : "here 4";
+  } else {
+    const userDoc: UserDocument | null = await UserModel.findById(_id);
+    return userDoc ?? "here 4";
   }
 }
