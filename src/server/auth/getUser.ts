46006d780df7ddd251cb9af41ea9c4cc332a401d
abundiko <abundiko111@gmail.com>
2023-08-24 05:11:51 +0100

    refactored isLoggedIn function

--- src/server/auth/getUser.ts
+++ src/server/auth/getUser.ts
@@ -5,16 +5,20 @@ import jwt from "jsonwebtoken";
 import { connectDB } from "../mongoose/init";
 import UserModel, { UserDocument } from "../mongoose/schemas/userSchema";
 
+/**
+ * Retrieves a user document from the database.
+ *
+ * @param {string | null} _id - The ID of the user document to retrieve. Defaults to null.
+ * @return {Promise<UserDocument | null>} The retrieved user document, or null if not found.
+ */
 export default async function getUser(
   _id: string | null = null
-): Promise<UserDocument | string> {
+): Promise<UserDocument | null> {
   await connectDB();
   if (!_id) {
     const token = cookies().get("_uid");
     const secret = process.env.JWT_SECRET || "";
-    if (!token) {
-      return "here 1";
-    }
+    if (!token) return null;
     try {
       const decoded = jwt.verify(token.value, secret) as any;
       const uid = decoded?._uid;
@@ -23,14 +27,14 @@ export default async function getUser(
       if (userData) {
         return userData;
       } else {
-        return "here 3";
+        return null;
       }
     } catch (error) {
       console.error("Failed to verify the token:", error);
-      return "here 2";
+      return null;
     }
   } else {
     const userDoc: UserDocument | null = await UserModel.findById(_id);
-    return userDoc ?? "here 4";
+    return userDoc ?? null;
   }
 }
--- src/server/auth/isLoggedIn.ts
+++ src/server/auth/isLoggedIn.ts
@@ -5,23 +5,32 @@ import { cookies } from "next/headers";
 import { connectDB } from "../mongoose/init";
 import UserModel from "../mongoose/schemas/userSchema";
 
+/**
+ * Checks if a user is logged in.
+ *
+ * @return {Promise<boolean>} - A promise that resolves to a boolean indicating if the user is logged in or not.
+ */
 export default async function isLoggedIn(): Promise<boolean> {
   const cookiesStore = cookies();
   const token = cookiesStore.get("_uid");
   const secret = process.env.JWT_SECRET || "";
-  if (!token) return false;
-  await jwt.verify(token.value, secret, async (error, decoded) => {
-    if (error) {
-      console.error("Failed to verify the token:", error);
-      return false;
-    } else {
-      const uid = (decoded as any)?._uid;
-      console.log("Original value:", uid);
-      await connectDB();
-      const userData = await UserModel.findById(uid);
-      if (userData) return true;
-      return false;
-    }
-  });
-  return true;
+
+  if (!token) {
+    return false;
+  }
+
+  try {
+    const decoded: any = await jwt.verify(token.value, secret);
+    const uid = decoded?._uid;
+
+    console.log("Original value:", uid);
+
+    await connectDB();
+    const userData = await UserModel.findById(uid);
+
+    return !!userData;
+  } catch (error) {
+    console.error("Failed to verify the token:", error);
+    return false;
+  }
 }
