4ceba181c3e95e02de489b6a51dc095230f7321d
abundiko <abundiko111@gmail.com>
2023-08-24 05:37:51 +0100

    editted Edit Page

--- src/app/(user)/user/edit/page.tsx
+++ src/app/(user)/user/edit/page.tsx
@@ -8,7 +8,7 @@ const EditUserPage = () => {
       <main className="border-r app-borders">
         <h1 className="page-title">Edit Profile</h1>
         <div className="p-4">
-          <div className="border app-borders light-bg rounded-lg p-3">
+          <div className="border app-borders light-bg rounded-lg p-3 md:w-8/12">
             <h6 className="text-md font-[600] mb-2">Profile Settings</h6>
           </div>
         </div>
