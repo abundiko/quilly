d5f3164ef43b168163fa6361d2763de93a849e85
abundiko <abundiko111@gmail.com>
2023-08-25 23:57:31 +0100

    updated layout of Interests page

--- src/app/(user)/user/edit/interests/page.tsx
+++ src/app/(user)/user/edit/interests/page.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/InterestButton";
 import { useRouter } from "next/navigation";
 import { testInterests } from "@/data/testInterests";
+import { FaCheckCircle } from "react-icons/fa";
 
 const SignupInterestsForm = () => {
   const { push: redirect } = useRouter();
@@ -31,43 +32,59 @@ const SignupInterestsForm = () => {
     }
   };
   return (
-    <main className="border-r app-borders">
-      <h1 className="page-title">Update Interests</h1>
-      <div className="p-4">
-        <form action={handleSubmit} className=" p-3 md:w-8/12 mx-auto">
-          <div className="h-fit max-h-[40vh] gap-1 flex flex-wrap w-full overflow-y-auto py-1 my-5">
-            {allInterests.map(
-              item =>
-                selectedInterests.includes(item)
-                  ? <InterestButtonSelected
-                      key={item}
-                      title={item}
-                      onClick={() => {
-                        setSelectedInterests(
-                          selectedInterests.filter(i => i != item)
-                        );
-                      }}
-                    />
-                  : <InterestButton
-                      key={item}
-                      title={item}
-                      onClick={() => {
-                        setSelectedInterests([...selectedInterests, item]);
-                      }}
-                    />
+    <form action={handleSubmit} className="border-r app-borders ">
+      <h1 className="page-title flex justify-between">
+        <span>Update Interests</span>
+        <button
+          disabled={selectedInterests.length < 3 ? true : isLoading}
+          className="app-btn py-1 px-2 rounded-3xl disabled:pointer-events-none disabled:opacity-50 text-sm"
+          name="signup-interests-submit"
+          type="submit"
+        >
+          {isLoading
+            ? <AppLoader />
+            : <div className="flex items-center gap-2">
+                <FaCheckCircle />
+                <span>Save</span>
+              </div>}
+        </button>
+      </h1>
+      <div className="p-4 ">
+        {
+          selectedInterests.length > 0 &&
+          <>
+        <h6 className="font-[600] opacity-80 text-sm">
+          Selected {selectedInterests.length}
+        </h6>
+        <div className="app-borders border flex flex-wrap gap-1 p-2 rounded-md mb-1">
+          {selectedInterests.map(item =>
+            <InterestButtonSelected
+              key={item}
+              title={item}
+              onClick={() => {
+                setSelectedInterests(selectedInterests.filter(i => i != item));
+              }}
+              />
+              )}
+        </div>
+              </>
+            }
+        <h6 className="font-[600] opacity-80 text-sm">Available</h6>
+        <div className="app-borders border flex flex-wrap gap-1 p-2 rounded-md">
+          {allInterests
+            .filter(item => !selectedInterests.includes(item))
+            .map(item =>
+              <InterestButton
+                key={item}
+                title={item}
+                onClick={() => {
+                  setSelectedInterests([...selectedInterests, item]);
+                }}
+              />
             )}
-          </div>
-          <button
-            disabled={selectedInterests.length < 3 ? true : isLoading}
-            className="app-btn w-full mb-5 disabled:pointer-events-none disabled:opacity-50"
-            name="signup-interests-submit"
-            type="submit"
-          >
-            {isLoading ? <AppLoader /> : "Create Account"}
-          </button>
-        </form>
+        </div>
       </div>
-    </main>
+    </form>
   );
 };
 
