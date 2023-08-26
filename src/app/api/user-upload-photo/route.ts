db365ee92b4181237887e2a33f18a29326d7aa7b
abundiko <abundiko111@gmail.com>
2023-08-26 13:05:03 +0100

    test for vercel 7

--- src/app/api/user-upload-photo/route.ts
+++ src/app/api/user-upload-photo/route.ts
@@ -8,80 +8,81 @@ import { unlink, writeFile } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-  try {
-    const data = await req.formData();
+  return NextResponse.json(["error", `${getFoldersInPath("../")} `]);
+  // try {
+  //   const data = await req.formData();
 
-    const _id = await getUserSessionId();
-    if (!_id) {
-      return NextResponse.json(["error", "Invalid User"]);
-    }
+  //   const _id = await getUserSessionId();
+  //   if (!_id) {
+  //     return NextResponse.json(["error", "Invalid User"]);
+  //   }
 
-    if (data.get("remove")) {
-      let userDoc = await UserModel.findById(_id);
-      if (userDoc && userDoc.img && userDoc.img.trim() != "") {
-        await unlink("./public" + userDoc.img);
-      }
-      userDoc = await UserModel.findByIdAndUpdate(
-        _id,
-        {
-          img: ""
-        },
-        { new: true }
-      );
-      if (userDoc) {
-        return NextResponse.json([
-          "success",
-          JSON.stringify(
-            {
-              ...userDoc.toObject(),
-              _id: userDoc._id.toString()
-            } as UserDocument
-          )
-        ]);
-      }
-    }
+  //   if (data.get("remove")) {
+  //     let userDoc = await UserModel.findById(_id);
+  //     if (userDoc && userDoc.img && userDoc.img.trim() != "") {
+  //       await unlink("./public" + userDoc.img);
+  //     }
+  //     userDoc = await UserModel.findByIdAndUpdate(
+  //       _id,
+  //       {
+  //         img: ""
+  //       },
+  //       { new: true }
+  //     );
+  //     if (userDoc) {
+  //       return NextResponse.json([
+  //         "success",
+  //         JSON.stringify(
+  //           {
+  //             ...userDoc.toObject(),
+  //             _id: userDoc._id.toString()
+  //           } as UserDocument
+  //         )
+  //       ]);
+  //     }
+  //   }
 
-    const file: File | null = (data.get("file") as unknown) as File;
+  //   const file: File | null = (data.get("file") as unknown) as File;
 
-    if (!file) return NextResponse.json(["error", "No file selected"]);
+  //   if (!file) return NextResponse.json(["error", "No file selected"]);
 
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-    const fileNameSplitted = file.name.split(".");
-    const fileExtension =
-      "." + fileNameSplitted[fileNameSplitted.length - 1].toLowerCase();
+  //   const bytes = await file.arrayBuffer();
+  //   const buffer = Buffer.from(bytes);
+  //   const fileNameSplitted = file.name.split(".");
+  //   const fileExtension =
+  //     "." + fileNameSplitted[fileNameSplitted.length - 1].toLowerCase();
 
-    if (![".png", ".jpg", ".jpeg", ".jfif"].includes(fileExtension))
-      return NextResponse.json(["error", "Select a png, jpg or jpeg Photo"]);
+  //   if (![".png", ".jpg", ".jpeg", ".jfif"].includes(fileExtension))
+  //     return NextResponse.json(["error", "Select a png, jpg or jpeg Photo"]);
 
-    const randomName = randomUUID();
-    const path = "../../../public/img/users/" + randomName + fileExtension;
-    await writeFile(path, buffer);
-    await connectDB();
-    let userDoc = await UserModel.findById(_id);
-    if (userDoc && userDoc.img && userDoc.img.trim() != "") {
-      await unlink("../../../public" + userDoc.img);
-    }
-    userDoc = await UserModel.findByIdAndUpdate(
-      _id,
-      { img: path.replace("../../../public", "") },
-      { new: true }
-    );
-    if (userDoc) {
-      return NextResponse.json([
-        "success",
-        JSON.stringify(
-          { ...userDoc.toObject(), _id: userDoc._id.toString() } as UserDocument
-        )
-      ]);
-    }
-    return NextResponse.json(["error", "connection error"]);
-  } catch (e) {
-    return NextResponse.json([
-      "error",
-      `${getFoldersInPath("../")} an error occurred ${e}`
-    ]);
-  }
+  //   const randomName = randomUUID();
+  //   const path = "../../../public/img/users/" + randomName + fileExtension;
+  //   await writeFile(path, buffer);
+  //   await connectDB();
+  //   let userDoc = await UserModel.findById(_id);
+  //   if (userDoc && userDoc.img && userDoc.img.trim() != "") {
+  //     await unlink("../../../public" + userDoc.img);
+  //   }
+  //   userDoc = await UserModel.findByIdAndUpdate(
+  //     _id,
+  //     { img: path.replace("../../../public", "") },
+  //     { new: true }
+  //   );
+  //   if (userDoc) {
+  //     return NextResponse.json([
+  //       "success",
+  //       JSON.stringify(
+  //         { ...userDoc.toObject(), _id: userDoc._id.toString() } as UserDocument
+  //       )
+  //     ]);
+  //   }
+  //   return NextResponse.json(["error", "connection error"]);
+  // } catch (e) {
+  //   return NextResponse.json([
+  //     "error",
+  //     `${getFoldersInPath("../")} an error occurred ${e}`
+  //   ]);
+  // }
 }
 
 function getFoldersInPath(path: string): string {
