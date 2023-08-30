3ed63321ca4d5faea00a4f3e50f418ceb4f1af92
abundiko <abundiko111@gmail.com>
2023-08-30 21:17:52 +0100

    changed head to proper way for next js app dir 2

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -11,3 +11,21 @@ export default function PublicLayout({children}:LayoutProps){
         <Footer />
     </>
 }
+
+export const metadata = {
+  title: "Quilly | Home",
+  desctiption: "Home of professionally crafted contents",
+  openGraph: {
+    title: "Quilly | Home",
+    description: "Home of professionally crafted contents",
+    images: [
+      {
+        url: "/img/hero-snapshot.png",
+        width: 800,
+        height: 600,
+        alt: "Quilly Logo"
+      }
+    ],
+    site_name: "Quilly"
+  }
+};
