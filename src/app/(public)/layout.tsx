cb9ed04b32bf6c610ad100342dd3ececeba7e56d
abundiko <abundiko111@gmail.com>
2023-08-30 22:07:35 +0100

    testing img api route for users profile images 6

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import { LayoutProps } from "../(auth)/signup/layout";
 import Footer from "@/components/Footer";
+import { Metadata } from "next";
 
 export default function PublicLayout({children}:LayoutProps){
   return <>
@@ -12,20 +13,33 @@ export default function PublicLayout({children}:LayoutProps){
     </>
 }
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Quilly | Home",
-  desctiption: "Home of professionally crafted contents",
+  description: "Home of professionally crafted contents",
   openGraph: {
     title: "Quilly | Home",
     description: "Home of professionally crafted contents",
+    siteName: "Quilly",
     images: [
       {
-        url: "/img/hero-snapshot.png",
-        width: 800,
-        height: 600,
-        alt: "Quilly Logo"
+        url: "https://quilly-blog.vercel.app/img/hero-snapshot.png",
+        width: 400,
+        height: 250,
+        alt: "Quilly Landing Page"
       }
     ],
-    site_name: "Quilly"
+  },
+  twitter:{
+    title: "Quilly | Home",
+    description: "Home of professionally crafted contents",
+    site:"Quilly",
+    images: [
+      {
+        url: "https://quilly-blog.vercel.app/img/hero-snapshot.png",
+        width: 400,
+        height: 250,
+        alt: "Quilly Landing Page"
+      }
+    ],
   }
 };
