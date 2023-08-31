3d770f6e72a4f00cf12eacf8d2c527b1b1782e6d
abundiko <abundiko111@gmail.com>
2023-08-31 16:16:44 +0100

    added animation to tos and contact pages

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -1,47 +1,52 @@
+"use client";
+
+import AnimatedPage from "@/components/AnimatedPage";
 import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function ContactPage() {
   return (
-    <main className="pt-20">
-      <section className="py-20">
-        <div className="container mx-auto text-center">
-          <h1 className=" text-3xl">
-            Developed By{" "}
-            <a
-              className="text-primary-dark dark:text-primary-light dim:text-primary-light hover:underline"
-              href="https://abundiko.netlify.app"
-            >
-              Abundance Ken-Dickson
-            </a>
-          </h1>
-          <div className="flex py-10 gap-3 justify-center items-center">
-            <a
-              className="text-3xl flex items-center justify-center rounded-md boder app-borders h-20 aspect-square hover:text-primary-dark dark:hover:text-primary-light dim:hover:text-primary-light hover:underline"
-              href="https://github.com/abundiko"
-            >
-              <FaGithub />
-            </a>
-            <a
-              className="text-3xl flex items-center justify-center rounded-md boder app-borders h-20 aspect-square hover:text-primary-dark dark:hover:text-primary-light dim:hover:text-primary-light hover:underline"
-              href="https://twitter.com/abundiko"
-            >
-              <FaTwitter />
-            </a>
-            <a
-              className="text-3xl flex items-center justify-center rounded-md boder app-borders h-20 aspect-square hover:text-primary-dark dark:hover:text-primary-light dim:hover:text-primary-light hover:underline"
-              href="https://www.linkedin.com/in/abundiko"
-            >
-              <FaLinkedin />
-            </a>
-            <a
-              className="text-3xl flex items-center justify-center rounded-md boder app-borders h-20 aspect-square hover:text-primary-dark dark:hover:text-primary-light dim:hover:text-primary-light hover:underline"
-              href="https://www.facebook.com/abundiko"
-            >
-              <FaFacebook />
-            </a>
+    <AnimatedPage>
+      <main className="pt-20">
+        <section className="py-20">
+          <div className="container mx-auto text-center">
+            <h1 className=" text-3xl">
+              Developed By{" "}
+              <a
+                className="text-primary-dark dark:text-primary-light dim:text-primary-light hover:underline"
+                href="https://abundiko.netlify.app"
+              >
+                Abundance Ken-Dickson
+              </a>
+            </h1>
+            <div className="flex py-10 gap-3 justify-center items-center">
+              <a
+                className="text-3xl flex items-center justify-center rounded-md boder app-borders h-20 aspect-square hover:text-primary-dark dark:hover:text-primary-light dim:hover:text-primary-light hover:underline"
+                href="https://github.com/abundiko"
+              >
+                <FaGithub />
+              </a>
+              <a
+                className="text-3xl flex items-center justify-center rounded-md boder app-borders h-20 aspect-square hover:text-primary-dark dark:hover:text-primary-light dim:hover:text-primary-light hover:underline"
+                href="https://twitter.com/abundiko"
+              >
+                <FaTwitter />
+              </a>
+              <a
+                className="text-3xl flex items-center justify-center rounded-md boder app-borders h-20 aspect-square hover:text-primary-dark dark:hover:text-primary-light dim:hover:text-primary-light hover:underline"
+                href="https://www.linkedin.com/in/abundiko"
+              >
+                <FaLinkedin />
+              </a>
+              <a
+                className="text-3xl flex items-center justify-center rounded-md boder app-borders h-20 aspect-square hover:text-primary-dark dark:hover:text-primary-light dim:hover:text-primary-light hover:underline"
+                href="https://www.facebook.com/abundiko"
+              >
+                <FaFacebook />
+              </a>
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </AnimatedPage>
   );
 }
--- src/app/(public)/terms-of-service/page.tsx
+++ src/app/(public)/terms-of-service/page.tsx
@@ -1,63 +1,70 @@
+"use client";
+
+import AnimatedPage from "@/components/AnimatedPage";
 export default function TermsOfService() {
   return (
-    <main className="pt-20">
-      <section className="py-10">
-        <div className="container  mx-auto px-8 ">
-          <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold">
-            Terms Of Service{" "}
-            <span className="text-sm app-text-error">(AI Generated)</span>
-          </h1>
-          <h2 className="font-bold mt-8 opacity-80 text-xl ">
-            User Responsibilities
-          </h2>
-          <p className="my-3 opacity-80 text-md md:pe-24">
-            Eligibility: You must be at least 13 years old to use Quilly. If you
-            are under the age of 18, you must have parental consent to use the
-            Platform.
-            <br />
-            <br />
-            Account Registration: You are responsible for providing accurate and
-            up-to-date information during the registration process. You must
-            keep your account credentials confidential and not share them with
-            any third party.
-            <br />
-            <br />
-            User Content: You are solely responsible for any content you post,
-            upload, or transmit on Quilly. You must ensure that your content
-            complies with applicable laws, does not infringe upon any
-            third-party rights, and is not offensive, defamatory, or misleading.
-            <br />
-            <br />
-            Prohibited Activities: You agree not to engage in any of the
-            following activities on Quilly:
-            <br />
-            <br />
-            Violating any applicable laws, regulations, or third-party rights.
-            Impersonating any person or entity. Harassing, bullying, or
-            intimidating others. Uploading or distributing malware, viruses, or
-            any other harmful code. Attempting to gain unauthorized access to
-          </p>
-          <h2 className="font-bold mt-8 opacity-80 text-xl">
-            Intellectual Property
-          </h2>
-          <p className="my-3 opacity-80 text-md md:pe-24">
-            Quilly Ownership: Quilly and its associated trademarks, logos, and
-            intellectual property are owned by the platform operator. You may
-            not use them without prior written permission.
-            <br />
-            <br />
-            User Content: By posting content on Quilly, you grant Quilly a
-            non-exclusive, worldwide, royalty-free license to use, reproduce,
-            modify, distribute, and publicly display your content on the
-            Platform.
-            <br />
-            <br />
-            Copyright Infringement: Quilly respects intellectual property
-            rights. If you believe that any content on the Platform infringes
-            your copyright, please contact us to report the infringement.
-          </p>
-        </div>
-      </section>
-    </main>
+    <AnimatedPage>
+      <main className="pt-20">
+        <section className="py-10">
+          <div className="container  mx-auto px-8 ">
+            <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold">
+              Terms Of Service{" "}
+              <span className="text-sm app-text-error">(AI Generated)</span>
+            </h1>
+            <h2 className="font-bold mt-8 opacity-80 text-xl ">
+              User Responsibilities
+            </h2>
+            <p className="my-3 opacity-80 text-md md:pe-24">
+              Eligibility: You must be at least 13 years old to use Quilly. If
+              you are under the age of 18, you must have parental consent to use
+              the Platform.
+              <br />
+              <br />
+              Account Registration: You are responsible for providing accurate
+              and up-to-date information during the registration process. You
+              must keep your account credentials confidential and not share them
+              with any third party.
+              <br />
+              <br />
+              User Content: You are solely responsible for any content you post,
+              upload, or transmit on Quilly. You must ensure that your content
+              complies with applicable laws, does not infringe upon any
+              third-party rights, and is not offensive, defamatory, or
+              misleading.
+              <br />
+              <br />
+              Prohibited Activities: You agree not to engage in any of the
+              following activities on Quilly:
+              <br />
+              <br />
+              Violating any applicable laws, regulations, or third-party rights.
+              Impersonating any person or entity. Harassing, bullying, or
+              intimidating others. Uploading or distributing malware, viruses,
+              or any other harmful code. Attempting to gain unauthorized access
+              to
+            </p>
+            <h2 className="font-bold mt-8 opacity-80 text-xl">
+              Intellectual Property
+            </h2>
+            <p className="my-3 opacity-80 text-md md:pe-24">
+              Quilly Ownership: Quilly and its associated trademarks, logos, and
+              intellectual property are owned by the platform operator. You may
+              not use them without prior written permission.
+              <br />
+              <br />
+              User Content: By posting content on Quilly, you grant Quilly a
+              non-exclusive, worldwide, royalty-free license to use, reproduce,
+              modify, distribute, and publicly display your content on the
+              Platform.
+              <br />
+              <br />
+              Copyright Infringement: Quilly respects intellectual property
+              rights. If you believe that any content on the Platform infringes
+              your copyright, please contact us to report the infringement.
+            </p>
+          </div>
+        </section>
+      </main>
+    </AnimatedPage>
   );
 }
