9db35f8a9dac59fc9d18c39a47c1f47a513c1962
abundiko <abundiko111@gmail.com>
2023-08-31 07:30:22 +0100

    edited supabase code

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,10 +5,6 @@ import { createClient } from "@supabase/supabase-js";
 const url = process.env.SUPABASE_URL || "";
 const key = process.env.SUPABASE_ANON_KEY || "";
 
-export const supabase = createClient(
-  "https://qkpmyljleclxdbfzafat.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrcG15bGpsZWNseGRiZnphZmF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMxNTM1NTIsImV4cCI6MjAwODcyOTU1Mn0.CVrPQiGh4D831z89FZu-RXM0t2oHH5LF-68B6T6y-nQ",
-  {
-    auth: { persistSession: false }
-  }
-);
+export const supabase = createClient(url, key, {
+  auth: { persistSession: false }
+});
