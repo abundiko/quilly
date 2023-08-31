76537112cfe52a52de3a21737c3ae477e5e825a5
abundiko <abundiko111@gmail.com>
2023-08-31 07:39:59 +0100

    edited supabase code 2

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.SUPABASE_URL || "";
-const key = process.env.SUPABASE_ANON_KEY || "";
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(url, key, {
   auth: { persistSession: false }
