954eab553be1e3f21aa044934275f6f898f82643
abundiko <abundiko111@gmail.com>
2023-08-31 07:28:47 +0100

    edited UserCard and getUsers function

--- src/components/home/UserCard.tsx
+++ src/components/home/UserCard.tsx
@@ -12,12 +12,12 @@ export default function UserCard({ full_name, username, img }: PostAuthor) {
       <Image
         src={formatImage(img)}
         alt={full_name}
-        height={30}
-        width={30}
+        height={40}
+        width={40}
         className="aspect-square rounded-full"
       />
       <div>
-        <h4 className="text-xl font-[600]">
+        <h4 className="text-base font-[600]">
           {full_name}
         </h4>
         <p className="text-sm opacity-80 ">
--- src/server/userActions/getUsers.ts
+++ src/server/userActions/getUsers.ts
@@ -4,7 +4,9 @@ import UserModel, { UserDocument } from "../mongoose/schemas/userSchema";
 
 export default async function getUsers(): Promise<UserDocument[] | null> {
   try {
-    const users = await UserModel.find();
+    const users = await UserModel.find().sort({
+      createdAt: -1
+    });
 
     if (users) {
       return users.map(item => {
